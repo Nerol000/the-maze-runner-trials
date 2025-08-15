e6dba9b50421b8e7f9a6bfac7d4cefbbf8e299d2
Nerol000 <lnerol000@gmail.com>
2025-08-14 17:42:09 -0700

    Added spawn egg

--- src/main/java/net/nerol/mazerunner/item/ModItems.java
+++ src/main/java/net/nerol/mazerunner/item/ModItems.java
@@ -3,12 +3,14 @@ package net.nerol.mazerunner.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.nerol.mazerunner.TheMazeRunner;
+import net.nerol.mazerunner.entity.ModEntities;
 import net.nerol.mazerunner.item.custom.FlareCureItem;
 import net.nerol.mazerunner.item.custom.FlareInjectorItem;
 
@@ -17,6 +19,9 @@ import java.util.function.Function;
 public class ModItems {
     public static final Item FLARE_CURE = registerItem("flare_cure", settings -> new FlareCureItem(settings.maxCount(1)));
     public static final Item FLARE_INJECTOR = registerItem("flare_injector", settings -> new FlareInjectorItem(settings.maxCount(1)));
+
+    public static final Item GRIEVER_SPAWN_EGG = registerItem("griever_spawn_egg", settings ->
+            new SpawnEggItem(ModEntities.GRIEVER, settings));
 
     private static Item registerItem(String name, Function<Item.Settings, Item> function) {
         return Registry.register(Registries.ITEM, Identifier.of(TheMazeRunner.MOD_ID, name),
